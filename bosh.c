a3357cdd889515143dc8e5e6872a9ad66083ce9b
Prasanth A R <raviprasanth45@gmail.com>
2021-02-21 07:14:24 +0530

    Added definitions and bosh development started

---
+++ bosh.c
@@ -0,0 +1,14 @@
+// header files
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "definitions.h"
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    bosh();
+    return 0;
+}
