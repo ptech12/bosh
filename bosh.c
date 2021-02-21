0700b9d66f5d0d3bf0756c567e6946b5f8b846fc
Prasanth A R <raviprasanth45@gmail.com>
2021-02-21 07:20:37 +0530

    Added function declarations

--- bosh.c
+++ bosh.c
@@ -4,11 +4,22 @@
 #include <stdlib.h>
 #include "definitions.h"
 
-
-
-
+// functions declarations
+// the starting of shell, bosh()
+void bosh();
+// function to read user commands
+char *read();
+// function to split or tokenizing the input
+char **split_into_tokens(char *);
+// heart of the bosh, the commands execution
+int bosh_execute(char **);
+// finally, exit the bosh
+int bosh_exit();
+// main function, and starting the shell
 int main(int argc, char const *argv[])
 {
     bosh();
     return 0;
 }
+
+
