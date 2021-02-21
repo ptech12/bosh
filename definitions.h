48eb49d19f51ed7a2c79dbfc661b43ad0d842056
Prasanth A R <raviprasanth45@gmail.com>
2021-02-21 08:06:09 +0530

    More declaration in definitions.h

--- definitions.h
+++ definitions.h
@@ -1,6 +1,6 @@
 // ANSII escape code, Color Red
-#define RED \033[0;31m
+#define RED "\033[0;31m"
 // delimiter for tokens or split_line()
 #define TOKEN_DEL " \t\r\n"
 // for reseting the current mode
-#define RESET \e[0m
+#define RESET "\e[0m"
